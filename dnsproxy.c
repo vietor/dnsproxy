d9adee4267c13d1871458850010d9ea027def482
Vietor Liu <vietor.liu@gmail.com>
2014-04-04 11:19:02 +0800

    mod: base code

--- dnsproxy.c
+++ dnsproxy.c
@@ -2,9 +2,53 @@
 
 void process_query(SOCKET server, char* buffer, int size, struct sockaddr_in *source)
 {
-	DNS_HDR* hdr;
+	int i, len;
+	DNS_HDR *hdr, *rhdr;
+	char *pos, *rear;
+	char domain[PACKAGE_SIZE];
+	char rbuffer[PACKAGE_SIZE];
 
 	hdr = (DNS_HDR*)buffer;
+	rhdr = (DNS_HDR*)rbuffer;
+	memset(rbuffer, 0, PACKAGE_SIZE);
+	rhdr->id = hdr->id;
+	rhdr->qr = 1;
+	// check header format
+	if(hdr->qr != 0 || hdr->tc != 0 || ntohs(hdr->q_count) != 1)
+		rhdr->rcode = 1;
+	else {
+		// analize query section
+		pos = buffer + sizeof(DNS_HDR);
+		rear = buffer + size;
+		i = 0;
+		memset(domain, 0, PACKAGE_SIZE);
+		while(pos < rear) {
+			len = (int)*pos++;
+			if(len < 0 || len > 63 || (pos + len) > (rear - sizeof(DNS_QES))) {
+				rhdr->rcode = 1;
+				break;
+			}
+			if(len > 0) {
+				if(i > 0)
+					domain[i++] = '.';
+				memcpy(domain + i, pos, len);
+				i+= len;
+				pos += len;
+			}
+			else {
+				DNS_QES *qes = (DNS_QES*) pos;
+				if(ntohs(qes->classes) != 0x01)
+					rhdr->rcode = 4;
+				else
+					pos += sizeof(DNS_QES);
+				break;
+			}
+		}
+	}
+	if(rhdr->rcode != 0) {
+		sendto(server, rbuffer, sizeof(DNS_HDR), 0, (struct sockaddr*)source, sizeof(struct sockaddr_in));
+		return;
+	}
 }
 
 int dnsproxy(unsigned int local_port, const char* remote_addr, unsigned int remote_port)
